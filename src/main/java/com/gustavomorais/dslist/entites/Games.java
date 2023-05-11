c8175c8998a1bf7fea233de9b84b83baba5d6555
Marianne <gustvmorais@gmail.com>
2023-05-11 11:11:42 -0300

    Game seed

--- src/main/java/com/gustavomorais/dslist/entites/Games.java
+++ src/main/java/com/gustavomorais/dslist/entites/Games.java
@@ -15,20 +15,22 @@ public class Games {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String plataforms;
+    private String platforms;
     private Double score;
     private String imgUrl;
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Games(){}
 
-    public Games(Long id, String title, Integer year, String genre, String plataforms, Double score, String imgUrl, String shortDescription, String longDescription) {
+    public Games(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.plataforms = plataforms;
+        this.platforms = platforms;
         this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
@@ -68,11 +70,11 @@ public class Games {
     }
 
     public String getPlataforms() {
-        return plataforms;
+        return platforms;
     }
 
     public void setPlataforms(String plataforms) {
-        this.plataforms = plataforms;
+        this.platforms = plataforms;
     }
 
     public Double getScore() {
